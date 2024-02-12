43b9a752d5c08aabdfa3ebfd4dd4ca71f53611f5
AhmedTeleb <ahmed.teleb@naqla.xyz>
2024-02-12 15:06:01 +0200

    implement ui design

---
+++ app/src/main/java/com/teleb/chefaaimagestask/presentation/details/DetailsFragment.kt
@@ -0,0 +1,29 @@
+package com.teleb.chefaaimagestask.presentation.details
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.teleb.chefaaimagestask.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class DetailsFragment : Fragment() {
+
+
+    private val viewModel: DetailsViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_details, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
--- app/src/main/java/com/teleb/chefaaimagestask/presentation/main/MainActivity.kt
+++ app/src/main/java/com/teleb/chefaaimagestask/presentation/main/MainActivity.kt
@@ -3,7 +3,9 @@ package com.teleb.chefaaimagestask.presentation.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.teleb.chefaaimagestask.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
